8002a889d64cc77e71cb9a7bbd40c2ff9c97aee1
Jashele T <jashele@Jasheles-Air.domain>
2019-12-01 13:30:51 -0700

    setup template for data access model

---
+++ schemes/recipes-model.js
@@ -0,0 +1,11 @@
+const db = require('../data/dbConfig.js')
+
+function getRecipes() {}
+function getShoppingList(recipe_id) {}
+function getInstructions(recipe_id) {}
+
+module.exports = {
+   getRecipes,
+   getShoppingList,
+   getInstructions
+}
