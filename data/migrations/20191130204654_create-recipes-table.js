0e3148a63a88c48dcac466a34312b21aa0e2f05f
Jashele T <jashele@Jasheles-Air.domain>
2019-12-01 11:06:53 -0700

    getting error: table recipe_ingredients has no column named recipe_id

--- data/migrations/20191130204654_create-recipes-table.js
+++ data/migrations/20191130204654_create-recipes-table.js
@@ -17,20 +17,44 @@ exports.up = function(knex) {
       // * * S T E P S  TABLE
       .createTable("steps", tbl => {
         tbl.increments();
-        tbl.integer("step_number").unsigned().notNullable();
+        tbl
+          .integer("step_number")
+          .unsigned()
+          .notNullable();
         tbl.string("instructions").notNullable();
         // FOREIGN KEY REFERENCE TO THE RECIPES TABLE - RECIPE ID
-        tbl.integer("recipe_id").unsigned().notNullable().references("recipes.id").onDelete("CASCADE").onUpdate("CASCADE");
+        tbl
+          .integer("recipe_id")
+          .unsigned()
+          .notNullable()
+          .references("recipes.id")
+          .onDelete("CASCADE")
+          .onUpdate("CASCADE");
       })
       // * * R E C I P E + I N G R E D I E N T S  TABLE
       .createTable("recipe_ingredients", tbl => {
         tbl.increments();
         // FOREIGN KEY REFERENCE TO THE RECIPES TABLE - RECIPE ID
-        tbl.integer("recipe_id").unsigned().notNullable().references("recipes.id").onDelete("CASCADE").onUpdate("CASCADE");
+        tbl
+          .integer("recipe_id")
+          .unsigned()
+          .notNullable()
+          .onDelete("CASCADE")
+          .onUpdate("CASCADE");
+          tbl.foreign('recipe_id').references('recipes.id')
         // FOREIGN KEY REFERENCE TO THE INGREDIENTS TABLE - INGREDIENT ID
-        tbl.integer("ingredient_id").unsigned().notNullable().references("ingredients.id").onDelete("CASCADE").onUpdate("CASCADE");
+        tbl
+          .integer("ingredient_id")
+          .unsigned()
+          .notNullable()
+          .onDelete("CASCADE")
+          .onUpdate("CASCADE");
+          tbl.foreign('ingredient_id').references('ingredients.id')
 
-        tbl.integer("ingredient_quantity", 255).unsigned().notNullable();
+        tbl
+          .integer("ingredient_quantity", 255)
+          .unsigned()
+          .notNullable();
         tbl.string("quantity_type", 255).notNullable();
       })
   );
--- data/seeds/04-recipe_ingredients.js
+++ data/seeds/04-recipe_ingredients.js
@@ -1,27 +1,15 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('recipe_ingredients').del()
+  return knex('recipe_ingredients').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('recipe_ingredients').insert([
         {
-          recipe_id: '1',
-          ingredient_id: '1',
-          ingredient_quantity: '12',
-          quantity_type: 'oz'
-        },
-        {
-          recipe_id: '1',
-          ingredient_id: '2',
-          ingredient_quantity: '1',
-          quantity_type: 'pounds'
-        },
-        {
-          recipe_id: '1',
-          ingredient_id: '1',
-          ingredient_quantity: '1',
-          quantity_type: 'can'
+          recipe_id: 1,
+          ingredient_id: 1,
+          ingredient_quantity: 1,
+          quantity_type: "OZ"
         }
       ]);
     });
