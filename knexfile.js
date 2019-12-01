4ac16208ceaf7cba3db27b553c056128f94e282c
Jashele T <jashele@Jasheles-Air.domain>
2019-11-30 20:10:03 -0700

    updated knexfile exports

--- knexfile.js
+++ knexfile.js
@@ -3,42 +3,14 @@
 module.exports = {
 
   development: {
+    // our DBMS driver
     client: 'sqlite3',
+    // the location of our db
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './data/recipes.db3', 
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    // necessary when using sqlite3
+    useNullAsDefault: true
   }
 
-};
+ };
